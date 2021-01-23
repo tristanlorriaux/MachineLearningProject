23ce899c5e9ae5a1836384af2d7c84e4f1b39a19
Gabin DURTESTE <58485625+gabindurteste@users.noreply.github.com>
2021-01-23 20:24:13 +0100

    Update pca_var_cum.py

--- Part2/pca_var_cum.py
+++ Part2/pca_var_cum.py
@@ -9,9 +9,9 @@ n_components = 100 #pour la PCA
 
 #PreProcessing des données
 
-X = np.load('C:/Users/titil/Desktop/MachineLearningProject/Part2/MNIST_X_28x28.npy')
-Y = np.load('C:/Users/titil/Desktop/MachineLearningProject/Part2/MNIST_y.npy')
-
+DATA_PATH = '' #Insérez le chemin des fichiers 
+X = np.load(DATA_PATH + '/MNIST_X_28x28.npy')
+Y = np.load(DATA_PATH + '/MNIST_y.npy')
 
 Xr= X.reshape(70000,784)/255.0 #On reshape les données
 
