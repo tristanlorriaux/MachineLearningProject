eb3c6a0c7853dea784cb234d7047e9eb36d503b3
Gabin DURTESTE <58485625+gabindurteste@users.noreply.github.com>
2021-01-23 20:19:21 +0100

    Add files via upload

--- Part2/em.py
+++ Part2/em.py
@@ -6,14 +6,15 @@ from sklearn.mixture import GaussianMixture
 import math
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
+from scipy.stats import mode
 
 n_components = 2
 k = 10
 
 #PreProcessing des données
-
-X = np.load('C:/Users/titil/Desktop/MachineLearningProject/Part2/MNIST_X_28x28.npy')
-Y = np.load('C:/Users/titil/Desktop/MachineLearningProject/Part2/MNIST_y.npy')
+DATA_PATH = 'C:/Users/Gabin Durteste/Downloads/MNIST' #Insérez le chemin des fichiers 
+X = np.load(DATA_PATH + '/MNIST_X_28x28.npy')
+Y = np.load(DATA_PATH + '/MNIST_y.npy')
 
 
 Xr= X.reshape(70000,784)/255.0 #On reshape les données
@@ -51,8 +52,13 @@ plt.show()
 #GM.fit(x_train)
 #print("La probabilité de mélange gaussien est de {}".format(math.exp(GM.score(x_test))))
 
+clusters = GM.predict(x_test)
+labels_em = np.zeros_like(clusters)
+for i in range(k):
+    mask_gauss = (clusters == i)
+    labels_em[mask_gauss] = mode(y_test[mask_gauss])[0]
+
 #Matrice de confusion
-labels_em = GM.predict(pca_test)
 mat_gm = confusion_matrix(y_test, labels_em)
 sns.heatmap(mat_gm.T, square=True, annot=True, fmt='d', cbar=False,
             xticklabels="0123456789",
